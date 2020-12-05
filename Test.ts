324979fde307b967546325be81e09ac306b3b844
GokulGit05 <gkgopig02@gmail.com>
2020-12-05 12:51:31 +0530

    Test

---
+++ Test.ts
@@ -0,0 +1,27 @@
+
+console.log("TypeScript Basics");
+
+// 1. Hello World
+var helloWorld = () => console.log("Printing Hello World form TS");
+helloWorld();
+
+// 2. Types Declaration. 
+
+// This is called post-fix declaration
+var numberType : number;
+var booleanType: boolean;
+var stringType : string;
+
+numberType = 10;
+// numberType = "10";
+
+booleanType = true;
+
+stringType = "Gokul";
+// stringType = true;
+console.log(numberType,booleanType,stringType);
+
+var undefinedType: undefined;
+var  nullType: null; // output also undefined
+
+console.log(undefinedType, nullType);   
